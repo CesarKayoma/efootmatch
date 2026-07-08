83b85591a525a89e6ed644bae4e6601a1baf4b67
Kayoma <cesarkayomacoma@gmail.com>
2026-07-08 11:37:29 -0300

    feat: :sparkles: Add initial application setup with Flask and database configuration

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from app.models import Goal, Match, Player, Team
+from app.extensions import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+
+    db.init_app(app)
+
+    return app
